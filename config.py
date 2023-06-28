3fc63dd8189a98ae1e0dee43263d5d31d6197574
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-06-28 21:54:52 +0400

    change errors, set up NO PROXY mode

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 TIME_DELAY = 0.01
 NUMBER_OF_PROXY = 11
+USE_PROXY = False # (here should be 'True' if you will use proxy or 'False' if not)
--- services.py
+++ services.py
@@ -6,7 +6,7 @@ import pickle
 import requests
 from fake_useragent import UserAgent
 import time
-from config import TIME_DELAY, NUMBER_OF_PROXY
+from config import TIME_DELAY, NUMBER_OF_PROXY, USE_PROXY
 
 
 def increase_number(stop_num=11):
@@ -22,9 +22,9 @@ def increase_number(stop_num=11):
 
     return increment
 
-
-generator1 = increase_number(11)
-generator2 = increase_number(NUMBER_OF_PROXY)
+if USE_PROXY:
+    request_counter = increase_number(11)
+    proxy_num = increase_number(NUMBER_OF_PROXY)
 
 
 def create_closure():
@@ -44,26 +44,30 @@ get_value_func, set_value_func = create_closure()
 
 
 def default_request(link, counter = 0):
-    global generator1
-    global get_value_func
-    global set_value_func
-    if generator1() == 0:
-        set_value_func(change_proxy())
-    proxy = get_value_func()
-    ip, port, login, password = proxy.split(':')
-    proxies = {
-        'http': f"http://{login}:{password}@{ip}:{port}",
-        'https': f"http://{login}:{password}@{ip}:{port}"
-    }
     ua = UserAgent()
     user_agent = ua.random
     headers = {
         'User-Agent': user_agent
     }
-    time.sleep(TIME_DELAY)
-    response = requests.get(url=link, headers=headers, proxies=proxies)
+    if USE_PROXY:
+        global request_counter
+        global get_value_func
+        global set_value_func
+        if request_counter() == 0:
+            set_value_func(change_proxy())
+        proxy = get_value_func()
+        ip, port, login, password = proxy.split(':')
+        proxies = {
+            'http': f"http://{login}:{password}@{ip}:{port}",
+            'https': f"http://{login}:{password}@{ip}:{port}"
+        }
+
+        time.sleep(TIME_DELAY)
+        response = requests.get(url=link, headers=headers, proxies=proxies)
+    else:
+        response = requests.get(url=link, headers=headers)
     print(link, response.status_code)
-    if response.status_code == 403 or response.status_code == 429 and counter != 3:
+    if (response.status_code == 403 or response.status_code == 429) and counter != 3:
         counter += 1
         print(link, f"try again ({counter})")
         default_request(link=link, counter=counter)
@@ -119,7 +123,7 @@ def create_pickle():
 
 
 def change_proxy():
-    global generator2
+    global proxy_num
     proxy_list = []
     with open('proxy.txt', 'r') as file:
         for line in file:
@@ -127,6 +131,9 @@ def change_proxy():
 
             if line:
                 proxy_list.append(line)
-    num = generator2()
+    num = proxy_num()
     proxy_list=proxy_list[1:]
-    return proxy_list[num]
+    if proxy_list:
+        return proxy_list[num]
+    else:
+        raise IndexError("You don't have proxy in 'proxy.txt'")
